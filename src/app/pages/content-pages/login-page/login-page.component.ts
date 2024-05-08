5cab07f41ad873455069213e4c1493df9c121b84
bekzatsa <bekzat.saylaubay@gmail.com>
2024-05-08 15:07:30 +0500

    upload

--- src/app/pages/content-pages/login-page/login-page.component.ts
+++ src/app/pages/content-pages/login-page/login-page.component.ts
@@ -1,12 +1,11 @@
 import {Component, Inject, Renderer2} from '@angular/core';
-import {NgbAlertModule} from "@ng-bootstrap/ng-bootstrap";
 import {DOCUMENT, NgIf} from "@angular/common";
 import {ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-test-page',
   standalone: true,
-  imports: [NgbAlertModule, ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
